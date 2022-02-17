2dbfe3bc8f366d138f6b23bd148435da7e3a201e
G1GNANAMANI <gnanamanichidambaram@gmail.com>
2022-02-17 09:37:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-nmwknr-mkrztu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-nmwknr-mkrztu)
---
+++ src/app/products.ts
@@ -0,0 +1,33 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'SHAMPOO',
+    price: 799,
+    description: 'GOOD NOURISHMENT FOR HAIR',
+  },
+  {
+    id: 2,
+    name: 'SOAP',
+    price: 699,
+    description: 'BEST FOR SKIN',
+  },
+  {
+    id: 3,
+    name: 'PERFUME',
+    price: 299,
+    description: 'A GOOD SENSE OF FRAGRANCE ',
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
